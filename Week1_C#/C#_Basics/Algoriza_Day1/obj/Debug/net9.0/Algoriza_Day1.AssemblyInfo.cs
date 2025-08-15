50f04df2512bf27c153633b9fdd98f0755e6ce4e
mohamedabdelstar06 <mohamedabdelstar06@gmail.com>
2025-08-16 02:00:50 +0300

    Add Day1 Reactjs

--- Week1_C#/C#_Basics/Algoriza_Day1/obj/Debug/net9.0/Algoriza_Day1.AssemblyInfo.cs
+++ Week1_C#/C#_Basics/Algoriza_Day1/obj/Debug/net9.0/Algoriza_Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Algoriza_Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24392a2ba89d477e302071a9b24ca38706a7b2de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86c7df20db7a9b8240e8610df99a05a3122123cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Algoriza_Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Algoriza_Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
